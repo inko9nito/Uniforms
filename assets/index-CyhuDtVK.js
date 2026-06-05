713846a219e6b7c187e3414d7e85477532d902d0
inko9nito <408521+inko9nito@users.noreply.github.com>
2026-06-05 17:02:31 +0000

    Deploying to gh-pages from @ inko9nito/Uniforms@5f942c0ab3b1e21f8df2e86427056e87d22a75b5 🚀

--- assets/index-CyhuDtVK.js
+++ assets/index-CyhuDtVK.js
@@ -83,7 +83,7 @@ the public listings can only be changed by you.
 | Boys | Navy polo (performance) | YM | Carrollton |  | 5 | 2 | images/polo-boys-navy-performance.jpg |  |
 | Boys | Gray polo | YM | Carrollton |  | 5 | 1 | images/polo-boys-gray.jpg |  |
 | Boys | Gray polo | YM | Carrollton | Marker blemish | 5 | 1 | images/polo-boys-gray.jpg |  |
-| Boys | White polo | YM | Carrollton |  | 5 | 1 | https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/80w/attribute_rule_images/1152615_source_1778859377.jpg |  |
+| Boys | White polo | YM | Carrollton |  | 5 | 1 | https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/1280x1280/products/78226/11090866/66611__76912.1778859374.jpg?c=1 |  |
 | Unisex | Spirit shirt | YL | Carrollton |  | 5 | 1 |  |  |
 | Unisex | Navy 1/4-Zip Performance Fleece Pullover | YL | Both |  | 5 | 1 | https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/320w/attribute_rule_images/1154203_source_1778859607.jpg |  |
 | Unisex | Gray Full-Zip Fleece Jacket | YL | Both |  | 5 | 1 | https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/320w/attribute_rule_images/1324430_source_1778756624.jpg |  |
